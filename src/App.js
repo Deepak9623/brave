eaef297c9490a72cc2afd558149a293111d5dced
Brave Fox <bravefoxofficial@gmail.com>
2023-06-05 10:09:38 +0530

    Added bootstrap dependencies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/Header';
+import { Button } from 'reactstrap';
 
 function App() {
-  return <h1>Service is currently down</h1>;
+  return (<div>
+    <Header></Header>
+    <Button>This </Button>
+  </div>);
 }
 
 export default App;
