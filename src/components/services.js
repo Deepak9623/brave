b7e84b01b6af6de65c235793c44a9cf22ea8c114
Brave Fox <bravefoxofficial@gmail.com>
2023-06-05 14:09:54 +0530

    sections

---
+++ src/components/services.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <p>Brave Fox has assembled teams of exceptional people. We take pride in being able to offer clients the best intellectual capital available to help them solve complex problems. When you work with Brave Fox, you work with knowledgeable professionals who bring valuable strategic thinking to the table along with a collaborative working style.
+
+        Our agile processes combined with our focus on the Microsoft and Oracle connected enterprise allow us to use information technology to effectively address changing business requirements for the modern-day enterprise. With the advent of new technologies and the use of the internet to achieve tighter integration between an organization and its customers and business partners, the potential of IT has only grown. Successful organizations will be able to realize that potential by building a new generation of IT systems and teaming up with a new generation of business partners like Brave Fox.
+        
+        Brave Fox key capabilities includes:
+        Brave Fox offers services across the entire product engineering lifecycle – from architectural assessments to post production support. Our clients leverage us for following functions:
+        IT Consulting
+        Quality Consulting
+        Software and Services Needs
+        IT Outsourcing
+        Architectural assessment
+        Prototypes & Proof of Concepts
+        Feature/Module/Product Development
+        Software Architecture & Design
+        Software Testing and Test Automation
+        Performance Engineering
+        Technology Migration to newer platforms
+        Database Migrations</p>
+    );
+}
+export default Services;
